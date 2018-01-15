3036b86d1ebb34743480d5f898f2aa7cd0e828ed
Yuki Nishizawa <freedom.ouest@gmail.com>
2018-01-15 23:10:33 +0900

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Yuki app';
+  members=[
+    { id: 1, name: "user1"},
+    { id: 2, name: "user2"},
+    { id: 3, name: "user3"},
+    { id: 4, name: "user4"},
+  ];
 }
