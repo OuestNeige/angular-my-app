12d8a781db5a43247e39fcfb11b3122b435aa51b
Yuki Nishizawa <freedom.ouest@gmail.com>
2018-01-16 13:18:55 +0900

    4-2

--- src/app/membertable.component.ts
+++ src/app/membertable.component.ts
@@ -3,8 +3,8 @@ import { Member } from './member';
 
 
 @Component({
-    selector: 'membertable'
-    templateUrl: `<h2>被選挙人名簿</h2>
+    selector: 'membertable',
+    template: `<h2>被選挙人名簿</h2>
     <ul>
         <li *ngFor="let member of members">
           <b>{{member.name}}</b>:&nbsp;{{member.prof}}
