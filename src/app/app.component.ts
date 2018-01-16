addd967d7246ad017edc6922bf795ea688481719
Yuki Nishizawa <freedom.ouest@gmail.com>
2018-01-16 13:26:28 +0900

    4-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { Member } from './member';
+//import { Member } from './member';
+import { MembertableComponent } from './membertable.component';
 
 @Component({
     selector: 'app-root',
     template: `<h1>{{name}}年度役員選挙</h1>
-  <h2>被選挙人名簿</h2>
-  <ul>
-      <li *ngFor="let member of members">
-          <b>{{member.name}}</b>:&nbsp;{{member.prof}}
-      </li>
-  </ul>`,
+      <membertable></membertable>`,
 })
 export class AppComponent {
     name = '2017';
-    members = Member.members;
 }
