fe67f232248797a4618b3a64d887e2c712424f44
Yuki Nishizawa <freedom.ouest@gmail.com>
2018-02-17 14:38:01 +0900

    6-2

--- src/poll.component.ts
+++ src/poll.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Member } from './member';
 
 
 @Component({
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
     styleUrls: ['./poll.component.css']
 })
 export class PollComponent {
+    findByVoted(voted: number): Member[] {
 
+        let foundMembers: Member[] = [];
+
+        for (let member of Member.members) {
+
+            if (member.voted == voted) {
+                foundMembers.push(member);
+            }
+        }
+
+        return foundMembers;
+    }
+
+    reportPoll(): Member[] {
+        let sortedMembers = (Member.members.slice()).sort(
+            (m1, m2) => m2.voted - m1.voted
+        );
+
+        let maxvote = (sortedMembers[0]).voted;
+
+        let membersToReport: Member[] = [];
+        for (let voted = maxvote; voted >= 0; voted--) {
+            let groupmembers: Member[] = this.findByVoted(voted);
+            if (groupmembers.length > 0) {
+                membersToReport = membersToReport.concat(groupmembers);
+            }
+            if (membersToReport.length >= 4) { break; }
+        }
+        return membersToReport;
+    }
 }
     
